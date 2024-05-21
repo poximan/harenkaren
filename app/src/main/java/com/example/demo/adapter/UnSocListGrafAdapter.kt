c1a6301320496ed450ca28e782aec955c9bcd0b8
Hugo Donato <hdonato@servicoop.com>
2024-05-21 13:55:24 -0300

    nada

--- app/src/main/java/com/example/demo/adapter/UnSocListGrafAdapter.kt
+++ app/src/main/java/com/example/demo/adapter/UnSocListGrafAdapter.kt
@@ -1,19 +1,100 @@
 package com.example.demo.adapter
 
-import android.content.Context
 import com.example.demo.model.UnidSocial
+import com.example.demo.viewModel.UnSocViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.UUID
 
-class UnSocListGrafAdapter(private val context: Context) {
+class UnSocListGrafAdapter(private val idRecorrido: UUID) {
 
-    internal fun setUnSoc(
-        unidSocialList: List<UnidSocial>
-    ) {
-        for (unidSocial in unidSocialList) {
-
+    fun graficar(unSocViewModel: UnSocViewModel): String {
+        var ret = ""
+        CoroutineScope(Dispatchers.IO).launch {
+            val unSocListAsync = unSocViewModel.readConFK(idRecorrido)
+            withContext(Dispatchers.Main) {
+                ret = generarHTML(unSocListAsync)
+            }
         }
+        return ret
     }
 
-    private fun armarResumen(unidSocial: UnidSocial): String {
-        return ""
+    private fun generarHTML(unidSocialList: List<UnidSocial>): String {
+
+        val staticHtmlIni = """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <script src="file:///android_asset/plotly-2.32.0.min.js"></script>
+            </head>
+            <body>
+            <div id="chart" style="width: 100%; height: 100%;"></div>
+            <script>
+        """.trimIndent()
+
+        // Parte dinámica del HTML (trazas)
+        val (traces, data) = generarCategorias(unidSocialList)
+        val dynamicHtml = StringBuilder()
+        for (trace in traces) {
+            dynamicHtml.append(trace).append("\n")
+        }
+
+        val dataStr = """
+            var data = [$data]
+            """.trimIndent()
+
+        // Parte estática final del HTML
+        val staticHtmlEnd = """
+
+                var layout = {
+                    barmode: 'stack',
+                };
+                Plotly.newPlot('chart', data, layout);
+            </script>
+            </body>
+            </html>
+        """.trimIndent()
+
+        // Combinar partes estáticas y dinámicas
+        return staticHtmlIni + dynamicHtml.toString() + dataStr + staticHtmlEnd
+    }
+
+    private fun generarCategorias(unidSocialList: List<UnidSocial>): Pair<List<String>, String> {
+
+        for (unidSocial in unidSocialList) {
+            val contadores = unidSocial.getContadores()
+            for (atribString in contadores) {
+                // obtengo un objeto Field
+                val valorAtributo = unidSocial.javaClass.getDeclaredField(atribString)
+                valorAtributo.isAccessible = true
+                // utilizar el objeto Field para obtener el valor del atributo en unidSocial.
+                val valor = valorAtributo.get(unidSocial)
+            }
+        }
+
+        val traceList = mutableListOf<String>()
+        val dataList = mutableListOf<String>()
+
+        // Generar 24 trazas
+        for (i in 1..24) {
+            val trace = """
+                
+            var trace$i = {
+                x: [20, 14, 23, 31, 8, 15],
+                y: ['reg6', 'reg5', 'reg4','reg3', 'reg2', 'reg1'],
+                text: ['vJuveniles', 'vDestetados', 'vCrias', 'vHembrasAd', 'vAlfaSams', 'vBeta'],
+                name: 'Trace $i',
+                orientation: 'h',
+                type: 'bar'
+            };
+        """.trimIndent()
+            val data = "trace$i"
+            traceList.add(trace)
+            dataList.add(data)
+        }
+        return Pair(traceList, dataList.joinToString(", "))
     }
 }
--- app/src/main/java/com/example/demo/fragment/list/UnSocListGrafFragment.kt
+++ app/src/main/java/com/example/demo/fragment/list/UnSocListGrafFragment.kt
@@ -15,10 +15,6 @@ import com.example.demo.R
 import com.example.demo.adapter.UnSocListGrafAdapter
 import com.example.demo.databinding.FragmentUnsocListGrafBinding
 import com.example.demo.viewModel.UnSocViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStreamWriter
@@ -38,113 +34,46 @@ class UnSocListGrafFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentUnsocListGrafBinding.inflate(inflater, container, false)
+        webView = binding.webView
 
         binding.homeActionButton.setOnClickListener { goHome() }
         binding.newUnsocButton.setOnClickListener { nuevaUnidadSocial() }
         binding.cambiarActionButton.setOnClickListener { cambiarVista() }
 
-        webView = binding.webView
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         webView.settings.javaScriptEnabled = true
         webView.settings.allowFileAccess = true
+
+        graficar()
+    }
+
+    private fun graficar() {
 
         val fileName = "index.html"
         val file = File(
             requireContext().filesDir,
             fileName
-        ) // Obtiene la referencia al directorio de archivos de la aplicación
+        )
 
         if (!file.exists()) {
             file.createNewFile()
         }
 
+        val unSocAdapter = UnSocListGrafAdapter(args.idRecorrido)
+        val dynamicHtml = unSocAdapter.graficar(unSocViewModel)
+
         val fileOutputStream = FileOutputStream(file)
         val outputStreamWriter = OutputStreamWriter(fileOutputStream)
 
-        val dynamicHtml = generateDynamicHtml()
         outputStreamWriter.write(dynamicHtml)
         outputStreamWriter.close()
 
         // Cargar el archivo HTML en el WebView
         webView.loadUrl("file:///${file.absolutePath}")
-
-        return binding.root
-    }
-
-    private fun generateDynamicHtml(): String {
-
-        val unSocAdapter = UnSocListGrafAdapter(requireContext())
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val unSocListAsync = unSocViewModel.readConFK(args.idRecorrido)
-            withContext(Dispatchers.Main) {
-                unSocAdapter.setUnSoc(unSocListAsync)
-            }
-        }
-
-        val staticHtmlIni = """
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="UTF-8">
-                <title>Plotly Stacked Horizontal Bar Chart</title>
-                <script src="file:///android_asset/plotly-2.32.0.min.js"></script>
-            </head>
-            <body>
-            <div id="chart" style="width: 100%; height: 100%;"></div>
-            <script>
-        """.trimIndent()
-
-        // Parte dinámica del HTML (trazas)
-        val (traces, data) = generateTraces()
-        val dynamicHtml = StringBuilder()
-        for (trace in traces) {
-            dynamicHtml.append(trace).append("\n")
-        }
-
-        val dataStr = """
-            var data = [$data]
-            """.trimIndent()
-
-        // Parte estática final del HTML
-        val staticHtmlEnd = """
-            
-            
-                var layout = {
-                    barmode: 'stack',
-                    title: 'Stacked Horizontal Bar Chart'
-                };
-
-                Plotly.newPlot('chart', data, layout);
-            </script>
-            </body>
-            </html>
-        """.trimIndent()
-
-        // Combinar partes estáticas y dinámicas
-        return staticHtmlIni + dynamicHtml.toString() + dataStr + staticHtmlEnd
-    }
-
-    private fun generateTraces(): Pair<List<String>, String> {
-        val traceList = mutableListOf<String>()
-        val dataList = mutableListOf<String>()
-
-        // Generar 24 trazas
-        for (i in 1..24) {
-            val trace = """
-                
-            var trace$i = {
-                x: [20, 14, 23],
-                y: ['Category A', 'Category B', 'Category C'],
-                name: 'Trace $i',
-                orientation: 'h',
-                type: 'bar'
-            };
-        """.trimIndent()
-            val data = "trace$i"
-            traceList.add(trace)
-            dataList.add(data)
-        }
-        return Pair(traceList, dataList.joinToString(", "))
     }
 
     private fun goHome() {
